96c355c1fe65ade3abb00cd8357689125b013cd2
JosueA-codegator <101157752+JosueA-codegator@users.noreply.github.com>
2022-03-18 14:18:02 -0400

    Update LoginPage.java
    login

--- src/test/java/pageObjects/LoginPage.java
+++ src/test/java/pageObjects/LoginPage.java
@@ -7,31 +7,34 @@ import org.openqa.selenium.support.PageFactory;
 import utilities.BaseClass;
 
 public class LoginPage extends BaseClass {
-	
-	@FindBy(id="username") WebElement uname;
-	@FindBy(id="password") WebElement pwd;
-	@FindBy(id="Login") WebElement loginBtn;
-	@FindBy(id="setupLink") WebElement setupLink;
-	
+
+	@FindBy(id = "username")
+	WebElement uname;
+	@FindBy(id = "password")
+	WebElement pwd;
+	@FindBy(id = "Login")
+	WebElement loginBtn;
+	@FindBy(id = "setupLink")
+	WebElement setupLink;
+
 	public LoginPage() {
 		PageFactory.initElements(driver, this);
 	}
-	
+
 	public void enterUsername(String username) {
 		uname.sendKeys(username);
 	}
-	
+
 	public void enterPassword(String password) {
 		pwd.sendKeys(password);
 	}
-	
+
 	public void clickLoginButton() {
 		loginBtn.click();
 	}
-	
+
 	public boolean setUpLinkPresent() {
 		return setupLink.isDisplayed();
 	}
-	
 
 }
