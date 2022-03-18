b84b68cf310b0bc50475a7c21aaa47515457e34b
Waqas Khan <wuikhan@gmail.com>
2022-03-17 20:57:58 -0400

    scenario for valid login

--- src/test/java/stepDefinitions/LoginPageTest.java
+++ src/test/java/stepDefinitions/LoginPageTest.java
@@ -1,12 +1,15 @@
 package stepDefinitions;
 
+import org.junit.Assert;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import pageObjects.LoginPage;
 import utilities.BaseClass;
 
 public class LoginPageTest extends BaseClass {
-	
+	LoginPage lp;
 	
 	@Given("I am on the {string} environment")
 	public void i_am_on_the_environment(String string) {
@@ -14,19 +17,26 @@ public class LoginPageTest extends BaseClass {
 	}
 	@Given("I enter {string} username")
 	public void i_enter_username(String string) {
-		 System.out.println("Hello");
+		lp = new LoginPage();
+		lp.enterUsername(string);
 	}
 	@Given("I enter {string} password")
 	public void i_enter_password(String string) {
-		 System.out.println("Hello");
+		lp = new LoginPage();
+		lp.enterPassword(string);
 	}
-	@When("I click the {string} button")
-	public void i_click_the_button(String string) {
-		 System.out.println("Hello");
+	
+	@When("I click the login button")
+	public void i_click_the_login_button() {
+		lp = new LoginPage();
+		lp.clickLoginButton();
 	}
+	
 	@Then("I should see the dashboad")
 	public void i_should_see_the_dashboad() {
-		 System.out.println("Hello");
+		lp = new LoginPage();
+		boolean present = lp.setUpLinkPresent();
+		Assert.assertTrue(present);
 	}
 	
 
